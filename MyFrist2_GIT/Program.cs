88ccdd0548a1b0414a7e50188ebf0b48f01f7580
jiyeonUning <yeon14735@gmail.com>
2024-07-18 15:53:59 +0900

    테스트
    깃 테스트를 위해 텍스트 한 줄을 추가했습니다.

--- MyFrist2_GIT/Program.cs
+++ MyFrist2_GIT/Program.cs
@@ -8,6 +8,8 @@
             string 직업 = Console.ReadLine();
 
             Console.WriteLine($"당신은 {name}이며, {직업}은 백수 입니다.");
+            Console.WriteLine("과연 이것도 반영할 수 있을까?");
+
         }
     }
 }
