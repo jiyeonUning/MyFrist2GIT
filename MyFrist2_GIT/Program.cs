5ef302056301b94f763d24ec82ce7470b7974729
jiyeonUning <yeon14735@gmail.com>
2024-07-18 12:42:20 +0900

    Naming
    이름과 직업으 설정해주었습니다.

--- MyFrist2_GIT/Program.cs
+++ MyFrist2_GIT/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace MyFrist2_GIT
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MyFrist2_GIT
 {
     internal class Program
     {
+        static Player()
+        {
+            public string name;
+            public string 직업 
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("헬로우 나이스투밋유!");
+            Console.WriteLine($"당신은 {name}이며, {직업}은 백수 입니다.");
         }
     }
 }
