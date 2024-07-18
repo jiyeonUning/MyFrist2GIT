6ea5f34d09e0444b347c83c3b0e91c5eb9861830
jiyeonUning <yeon14735@gmail.com>
2024-07-18 14:29:11 +0900

    string text
    이름과 직업을 입력할 수 있게 해주었다.

--- MyFrist2_GIT/Program.cs
+++ MyFrist2_GIT/Program.cs
@@ -1,19 +1,14 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace MyFrist2_GIT
+﻿namespace MyFrist2_GIT
 {
     internal class Program
     {
-        static Player()
-        {
-            public string name;
-            public string 직업 
-        }
-
-
         static void Main(string[] args)
         {
+            string name = Console.ReadLine();
+            string 직업 = Console.ReadLine();
+
             Console.WriteLine($"당신은 {name}이며, {직업}은 백수 입니다.");
         }
     }
 }
+
